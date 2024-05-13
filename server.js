ebb1a54e5a55664a8b3e5686dda163f887cf8017
C-gyeltshen <02230279.cst@rub.edu.bt>
2024-05-14 01:08:04 +0600

    ok

--- server.js
+++ server.js
@@ -11,20 +11,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.post('/create/login',async(req,res)=>{
-    const {login_id,player_id,login_time,device_info,ip_address,login_status} = req.body;
+    // const {login_id,player_id,login_time,device_info,ip_address,login_status} = req.body;
+    const {id,DeviceInfo,IPAddress,LoginID_Pk_,LoginSuccess,LoginTimestamp,PlayerID_FK_} = req.body;    
     const result = await prisma.player_login.create({
         data:{
-            login_id:login_id,
-            player_id:player_id,
-            login_time:login_time,
-            device_info:device_info,
-            ip_address:ip_address,
-            login_status:login_status
+            id:id,
+            DeviceInfo:DeviceInfo,
+            IPAddress:IPAddress,
+            LoginId_PK_:LoginID_Pk_,
+            LoginSuccess:LoginSuccess,
+            LoginTimestamp:LoginTimestamp,
+            PlayerID_FK_:PlayerID_FK_
         },
     });
     res.json(result);
 });
+app.get('/ok', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
