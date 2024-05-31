0414297f43622c2f6bf67a99dd1737c9f291a47c
C-gyeltshen <02230279.cst@rub.edu.bt>
2024-06-01 03:28:45 +0600

    crud is implemented

--- server.js
+++ server.js
@@ -14,31 +14,85 @@ app.use((req, res, next) => {
     next();
 });
 
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/create/user_login', async(req,res)=>{
-    const {login_id,player_id,login_time,device_info,ip_address,login_success} =  req.body
-    const result = await prisma.player_login.create({
-        data : {
-            DeviceInfo:device_info,    
-            IPAddress:ip_address,  
-            LoginId_PK_:login_id,
-            LoginSuccess:login_success,
-            LoginTimestamp:login_time,
-            PlayerID_FK_:player_id
-            },
-        });
+app.get("/records", async (req, res) => {
+    const result = await prisma.player_login.findMany();
     res.json(result);
-});
+    });
 
-
-
-
+//Read data from server 
 app.get('/ok', (req, res) => {
     res.send('Hello World!')
 })
+
+//Creating a new data in db
+app.post("/player/login", async (req, res) => {
+    const { login_id,player_id,login_time,device_info,ipaddress,login_success } = req.body;
+    const result = await prisma.player_login.create({
+        data: {
+            login_id:login_id,
+            player_id:player_id,
+            login_time:login_time,
+            device_info:device_info,
+            ipaddress:ipaddress,
+            login_success:login_success
+        },
+    });
+    res.json(result);
+    });
+
+    app.post("/player/info", async (req, res) => {
+        const { player_id,player_name, player_level, exp, resources, trophies } = req.body;
+        const result = await prisma.players.create({
+            data: {
+                player_id:player_id,
+                player_name:player_name,
+                player_level:player_level,
+                exp:exp,
+                player_resources:resources,
+                player_trophies:trophies
+            },
+        });
+        res.json(result);
+        });
+
+// DELETE A SPECIFIC TASK BY ID
+app.delete("/records/delete", async (req, res) => {
+    const { login_id } = req.body;
+    const result = await prisma.player_login.delete({
+        where: {
+        login_id: login_id,
+        },
+    });
+    res.json({ result });
+    });
+
+// Read data from db
+app.get("/records/read", async(req,res)=>{
+    try{
+        const result = await prisma.player_login.findMany();
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+// UPDATE TASK TO BE COMPLETED OR NOT COMPLETED
+app.patch("/records/update", async (req, res) => {
+    const { player_id, player_name } = req.body;
+    console.log(player_id, player_name);
+    const result = await prisma.players.update({
+        where: { player_id: player_id },
+        data: {
+        player_name: player_name,
+        },
+    });
+    res.json(result);
+    });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
