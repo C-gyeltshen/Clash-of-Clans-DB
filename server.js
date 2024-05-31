cc36e6ca7087aaf04d621c13ef8d9c85c28aa255
C-gyeltshen <02230279.cst@rub.edu.bt>
2024-05-31 22:40:48 +0600

    implementing the REST api

--- server.js
+++ server.js
@@ -7,25 +7,35 @@ const app = express()
 app.use(bodyParser.json()); // for parsing application/json
 const port = 3000
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow the GET, POST, OPTIONS methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow Content-Type header
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.post('/create/login',async(req,res)=>{
-    // const {login_id,player_id,login_time,device_info,ip_address,login_status} = req.body;
-    const {id,DeviceInfo,IPAddress,LoginID_Pk_,LoginSuccess,LoginTimestamp,PlayerID_FK_} = req.body;    
+
+app.post('/create/user_login', async(req,res)=>{
+    const {login_id,player_id,login_time,device_info,ip_address,login_success} =  req.body
     const result = await prisma.player_login.create({
-        data:{
-            id:id,
-            DeviceInfo:DeviceInfo,
-            IPAddress:IPAddress,
-            LoginId_PK_:LoginID_Pk_,
-            LoginSuccess:LoginSuccess,
-            LoginTimestamp:LoginTimestamp,
-            PlayerID_FK_:PlayerID_FK_
-        },
-    });
+        data : {
+            DeviceInfo:device_info,    
+            IPAddress:ip_address,  
+            LoginId_PK_:login_id,
+            LoginSuccess:login_success,
+            LoginTimestamp:login_time,
+            PlayerID_FK_:player_id
+            },
+        });
     res.json(result);
 });
+
+
+
+
 app.get('/ok', (req, res) => {
     res.send('Hello World!')
 })
